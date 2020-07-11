fe2a96ba7c5417e8a9207a3d1a5f34f96e2467f5
Johan von Hacht <johvh@kth.se>
2020-07-11 19:12:45 +0200

    add navbar and move analysis end-point #8

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 from flask import Flask, escape, request, render_template, make_response, redirect, url_for
 app = Flask(__name__)
 
-
 @app.route('/')
 def main():
-    return render_template('index.html')
+    return 'todo'
+
+@app.route('/analys')
+def utdelning():
+    return render_template('analys.html')
+
+@app.route('/faq')
+def faq():
+    return 'todo'
+
+@app.route('/instruktioner')
+def instructions():
+    return 'todo'
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
